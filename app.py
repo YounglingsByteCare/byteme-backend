a7c4637d0414de57e4dc68b78d713d3c98f8443b
Adam <adambanderker50@gmail.com>
2021-01-17 12:51:52 +0200

    Added Contact us form

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
-from flask_mail import Mail
+from flask_mail import Mail, Message
+from resources.forms import ContactForm
 
 from database.db import initialize_db
 from flask_restful import Api
@@ -25,7 +26,6 @@ mail = Mail(app)
 # imports requiring app and mail
 from resources.routes import initialize_routes
 
-api = Api(app)
 api = Api(app, errors=errors)
 bcrypt = Bcrypt(app)
 jwt = JWTManager(app)
@@ -35,5 +35,16 @@ app.config['MONGODB_SETTINGS'] = {
     'host': 'mongodb+srv://byteme:1234@cluster0.mlj40.mongodb.net/test?retryWrites=true&w=majority'
 }
 
+@app.route('/contact', methods=['POST'])
+def contact():
+    form = ContactForm()
+    msg = Message(form.subject.data, sender='contact@example.com', recipients=['bytecare0@gmail.com'])
+    msg.body = """
+    From: %s
+    Email: <%s>
+    Message: %s
+    """ % (form.name.data, form.email.data, form.message.data)
+    mail.send(msg)
+    return 'Form posted.'
 initialize_db(app)
 initialize_routes(api)
