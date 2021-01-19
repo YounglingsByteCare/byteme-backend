a8b041349680bebf58605c4a2a400cabcb69cf1e
Adam <adambanderker50@gmail.com>
2021-01-19 12:38:03 +0200

    updated

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-from flask import Flask, render_template, request
-from chatterbot import ChatBot
-from chatterbot.trainers import ChatterBotCorpusTrainer
+from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
 from flask_mail import Mail, Message
@@ -31,7 +29,7 @@ from resources.routes import initialize_routes
 api = Api(app, errors=errors)
 bcrypt = Bcrypt(app)
 jwt = JWTManager(app)
-bot = ChatBot("ByteBot")
+
 
 
 app.config['MONGODB_SETTINGS'] = {
@@ -49,20 +47,6 @@ def contact():
     """ % (form.name.data, form.email.data, form.message.data)
     mail.send(msg)
     return 'Form posted.'
-
-trainer =  ChatterBotCorpusTrainer(bot)
-# trainer.train({'What is your name?':'My name is Candice'})
-#trainer.train("chatterbot.corpus.english")
-#trainer.train("data/greetings.yml")
-trainer.train("data/data.yml")
-
-@app.route("/chatbot")
-def home():
-    return render_template("home.html")
-@app.route("/get")
-def get_bot_response():
-    userText = request.args.get('msg')
-    return str(bot.get_response(userText))
 
 initialize_db(app)
 initialize_routes(api)
