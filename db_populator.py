bd73d1ea55fcd2aeff741d7c16e8772faf351d1b
Jesse <jesseb727@gmail.com>
2021-01-20 23:09:20 +0200

    Included Hospital & Appointment data generators.

--- db_populator.py
+++ db_populator.py
@@ -1,6 +1,7 @@
 import json
 import random
 import requests
+from datetime import datetime
 
 url = "http://127.0.0.1:5000/api/"
 
@@ -23,6 +24,10 @@ userdata_to_create = [
                 "postal_code": "7530"
             }, "0865001215"),
         ],
+        "appointments": [
+            ("General", "Stomach Bug"),
+            ("Semi-private", "COVID-19")
+        ]
     },
     {
         "user": ("tomato@gmail.com", "tomato"),
@@ -42,6 +47,10 @@ userdata_to_create = [
                 "postal_code": "5555"
             }, "0155466897"),
         ],
+        "appointments": [
+            ("General", "Diabetes"),
+            ("Private", "Heart Attack")
+        ]
     }
 ]
 admindata_to_create = [
@@ -63,6 +72,11 @@ admindata_to_create = [
                 "postal_code": "1234"
             }, "0811555598"),
         ],
+        "hospitals": [
+            ("Groote Schuur Hospital", [-33.94081566091405, 18.4616755829101]),
+            ("Melomed Bellville Private Hospital",
+             [-33.901385762815195, 18.624403298254883])
+        ]
     },
     {
         "user": ("Chocolate", "chocolate@gmail.com", "choolate"),
@@ -82,6 +96,13 @@ admindata_to_create = [
                 "postal_code": "1234"
             }, "0555700552"),
         ],
+        "hospitals": [
+            ("Karl Bremer Hospital",
+             [-33.891870448304935, 18.609708891065416]),
+            ("Mediclinic Louis Leipoldt",
+             [-33.90009911632751, 18.613056288015194]),
+            ("Mediclinic Panorama", [-33.87518210873116, 18.576183400259424])
+        ]
     },
     {
         "user": ("Cookies", "cookies@gmail.com", "cookies"),
@@ -101,6 +122,10 @@ admindata_to_create = [
                 "postal_code": "1234"
             }, "0605559927"),
         ],
+        "hospitals": [
+            ("Netcare N1 City Hospital",
+             [-33.89320714098042, 18.558594069568404])
+        ]
     },
     {
         "user": ("Biscuit", "biscuit@gmail.com", "biscuit"),
@@ -120,10 +145,13 @@ admindata_to_create = [
                 "postal_code": "1234"
             }, "0835553106"),
         ],
+        "hospitals": [
+            ("Netcare UCT Private Academic Hospital",
+             [-33.94159234030289, 18.462903977495348]),
+            ("Netcare Kuilsriver Hospital",
+             [-33.91735414153332, 18.67510024629595])
+        ]
     }
-]
-hospital_data_to_create = [
-    ("Groote Schuur Hospital")
 ]
 
 
@@ -214,9 +242,52 @@ def create_admin(jwt_token, name, surname, gender, id_number, address, phone_num
         "phone": phone_number,
     }
 
-    patients_url = url + "admins"
+    admin_url = url + "admins"
 
-    resp = requests.post(patients_url, json=data, headers=headers)
+    resp = requests.post(admin_url, json=data, headers=headers)
+
+    if resp.status_code == 200:
+        return resp.json()["id"]
+    else:
+        raise Exception(resp.content)
+
+
+def create_hospital(jwt_token, hospital_name, coordinates):
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {jwt_token}"
+    }
+    data = {
+        "hospital_name": hospital_name,
+        "point": coordinates
+    }
+
+    hospital_url = url + "hospitals"
+
+    resp = requests.post(hospital_url, json=data, headers=headers)
+
+    if resp.status_code == 200:
+        return resp.json()["id"]
+    else:
+        raise Exception(resp.content)
+
+
+def create_appointment(jwt_token, patient_id, hospital_id, date, ward, reason):
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {jwt_token}"
+    }
+    data = {
+        "patient_selected": patient_id,
+        "hospital_selected": hospital_id,
+        "appointment_date": date,
+        "ward_type": ward,
+        "reason_for_visit": reason
+    }
+
+    appointments_url = url + "appointments"
+
+    resp = requests.post(appointments_url, json=data, headers=headers)
 
     if resp.status_code == 200:
         return resp.json()["id"]
@@ -239,19 +310,28 @@ if __name__ == "__main__":
             user_data[0], user_data[1], user_data[2]
         )
 
-        adminuser_results[user_data[1]] = token
+        if user_data[1] not in adminuser_results:
+            adminuser_results[user_data[1]] = {}
+        adminuser_results[user_data[1]]["jwt_token"] = token
+        adminuser_results[user_data[1]]["admins"] = []
+        adminuser_results[user_data[1]]["hospitals"] = []
 
         for admin in data_item["admins"]:
-            print(f'─ {admin[0]}\t\t[ ]')
             admin_id = create_admin(
                 token, admin[0], admin[1], admin[2], admin[3], admin[4], admin[5]
             )
-            print(f'\r─ {admin[0]}\t\t[✓]')
+            adminuser_results[user_data[1]]["admins"].append(admin_id)
+            print(f'─ Admin: {admin[0]}\t\t[✓]')
+
+        for hospital in data_item["hospitals"]:
+            hospital_id = create_hospital(token, hospital[0], hospital[1])
+            adminuser_results[user_data[1]]["hospitals"].append(hospital_id)
+            print(f'─ Hospital: {hospital[0]}\t\t[✓]')
 
         print('')
         admin_user_count += 1
 
-    user_count = 0
+    user_count = 1
     print('Creating Users')
 
     for data_item in userdata_to_create:
@@ -260,12 +340,35 @@ if __name__ == "__main__":
         user_data = data_item["user"]
         token = create_and_login_user(user_data[0], user_data[1])
 
-        user_results[user_data[0]] = token
+        if user_data[1] not in user_results:
+            user_results[user_data[1]] = {}
+        user_results[user_data[1]]["jwt_token"] = token
+        user_results[user_data[1]]["patients"] = []
+        user_results[user_data[1]]["appointments"] = []
 
         # Next, create the patient data.
         for patient in data_item["patients"]:
-            print(f'─ {admin[0]}\t\t[ ]')
             patient_id = create_patient(
                 token, patient[0], patient[1], patient[2], patient[3], patient[4], patient[5]
             )
-            print(f'\r─ {admin[0]}\t\t[✓]')
+            user_results[user_data[1]]["patients"].append(patient_id)
+            print(f'─ {patient[0]}\t\t[✓]')
+
+        for appointment in data_item["appointments"]:
+            index = random.choice(admindata_to_create)["user"][1]
+            print(index)
+
+            appointment_id = create_appointment(
+                token,
+                random.choice(
+                    adminuser_results[index]["hospitals"]
+                ),
+                random.choice(user_results[user_data[1]]["patients"]),
+                datetime.utcnow().strftime('%Y-%m-%dT%H:%M'),
+                appointment[0],
+                appointment[1]
+            )
+            print(f'─ {appointment[1]}\t\t[✓]')
+
+        print('')
+        user_count += 1
